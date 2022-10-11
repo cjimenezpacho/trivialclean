de176ec3594ef514543683934852c343e213ce6a
Carlos Jiménez <carlos.jimenez.pacho@gmail.com>
2022-10-11 20:02:36 +0200

    adding more folders and api datasource interfaz and empty implementation

---
+++ TrivialClean/Data/Network/OpenTriviaAPIDataSourceImp.swift
@@ -0,0 +1,12 @@
+//
+//  OpenTriviaAPIDataSourceImp.swift
+//  TrivialClean
+//
+//  Created by Carlos Personal on 11/10/22.
+//
+
+import Foundation
+
+class OpenTriviaAPIDataSourceImp: OpenTriviaAPIDataSource {
+    
+}
