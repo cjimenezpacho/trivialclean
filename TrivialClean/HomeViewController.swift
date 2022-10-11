6511f4552d306d07bee1284d42c8303a6be2c8de
Carlos Jiménez <carlos.jimenez.pacho@gmail.com>
2022-10-11 19:10:47 +0200

    setup project

---
+++ TrivialClean/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TrivialClean
+//
+//  Created by Carlos Personal on 11/10/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
